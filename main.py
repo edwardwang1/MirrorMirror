03bad9fe2e9614973999503212dea9b2d95bb3f5
Edward Wang <Edward Wang>
2016-05-18 18:10:32 -0700

    testing pyqt4 on rpi

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+"""
+ZetCode PyQt4 tutorial
+
+In this example, we create a simple
+window in PyQt4.
+
+author: Jan Bodnar
+website: zetcode.com
+last edited: October 2011
+"""
+
 import sys
-from PyQt4 import QtGui4
+from PyQt4 import QtGui
 
 
 def main():
 
-    app = QtGui4.QApplication(sys.argv)
+    app = QtGui.QApplication(sys.argv)
 
-    w = QtGui4.QWidget()
+    w = QtGui.QWidget()
     w.resize(250, 150)
     w.move(300, 300)
     w.setWindowTitle('Simple')
